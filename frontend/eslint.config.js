b85f80ad2d71f57a4df0a273656e38d3bde42998
linhhn508 <linhhn508@gmail.com>
2026-09-10 23:30:21 +0000

    [frontend] add linting

---
+++ frontend/eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierConfig from 'eslint-plugin-prettier/recommended';
+import { globalIgnores } from 'eslint/config';
+
+export default [
+  globalIgnores(['dist']),
+  js.configs.recommended,
+  react.configs.flat.recommended,
+  reactHooks.configs.flat['recommended-latest'],
+  reactRefresh.configs.vite,
+  prettierConfig,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': [
+        'error', 
+        { 
+          singleQuote: true,
+          printWidth: 100,
+          tabWidth: 2,
+          semi: true,
+          trailingComma: 'es5',
+          bracketSpacing: true,
+          endOfLine: 'lf'
+        }],
+    },
+  },
+    {
+      files: ['**/*.{test,spec}.{js,jsx}', 'tests/**/*.{js,jsx}'],
+      languageOptions: {
+        globals: {
+          beforeAll: 'readonly',
+          beforeEach: 'readonly',
+          afterAll: 'readonly',
+          afterEach: 'readonly',
+          describe: 'readonly',
+          expect: 'readonly',
+          it: 'readonly',
+          test: 'readonly',
+        },
+      },
+    },
+];
